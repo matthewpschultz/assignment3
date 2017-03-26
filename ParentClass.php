c2d3ef54a473d6b91b6565083632049b1f1d74b7
matthewpschultz <matthewpschultz@yahoo.com>
2017-03-26 19:50:11 -0400

    completed form to select monster and to call basic first monster encounter

--- ParentClass.php
+++ ParentClass.php
@@ -1,6 +1,40 @@
 <?php
 	// This is the file for the parent class
 
-	class ParentClass {
-		
+	class Monster {
+
+		var $flies;
+		var $fangs;
+		var $hair;
+		var $alignment;
+
+		function __construct ($flies, $fangs, $hair, $alignment)
+		{
+			$this->flies = $flies;
+			$this->fangs = $fangs;
+			$this->hair = $hair;
+			$this->alignment = $alignment;
+		}
+
+		public function can_fly()
+		{
+			return $this->flies;
+		}
+
+		function has_fangs()
+		{
+			return $this->fangs;
+		}
+
+		public function how_hairy()
+		{
+			return $this->hair;
+		}
+
+		function its_alignment()
+		{
+			return $this->alignment;
+		}
+
 	}
+?>
--- assignment3.php
+++ assignment3.php
@@ -1,7 +1,5 @@
-<?php 
-
-	
-
+<?php
+include "ParentClass.php";
 ?>
 
 <!doctype html>
@@ -13,6 +11,38 @@
 	<link rel="stylesheet" href="assignment3.css">
 </head>
 <body>
-	
+	<div id = "wrap">
+	  <form action="" name="monsterPicker" method="post">
+			<input type="radio" name="buttons" value="1" > 1
+			<input type="radio" name="buttons" value="2" > 2
+	  	<input type="radio" name="buttons" value="3" > 3
+	  	<input type="radio" name="buttons" value="4" > 4
+			<input type="submit" name="submit" value="Pick a Monster!" />
+		</form>
+		<?php
+
+			if(isset($_POST['buttons']))
+			{
+					echo "You have picked monster ", $_POST['buttons'], "!";
+					$monsterNumber = $_POST['buttons'];
+			}
+
+			// Displays the encounter with your selected monster and its attributes
+			if ($monsterNumber == "1")
+			{
+				$monster1 = new Monster("does not", "does not", "is very", "neutral");
+				echo nl2br ("\n");
+				echo "you encounter monster 1 on the path! It ", $monster1->flies, " fly";
+			}
+
+
+
+			?>
+
+
+
+
+
+	</div>
 </body>
 </html>
